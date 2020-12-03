64b390fea91e95b3626f68ec4fb0f986f57db321
Dominik Schneider <dominik.schneider@wsu.edu>
2020-12-02 18:36:44 -0800

    convert forked project to cppc template

--- README.md
+++ README.md
@@ -8,7 +8,7 @@ Requires [Cookiecutter](http://cookiecutter.readthedocs.io/) version 1.4 or grea
 ## Usage
 
 The bulk of files required to start an image analysis project can be generated using
-the command below.
+the command below. You need to have git and cookiecutter installed.
 
 ```
 $ cookiecutter gh:CougPhenomics/cookiecutter-phenomics
---
+++ {{cookiecutter.project}}/build_docker.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# Run with: make build
+
+# back up the environment.yml file just in case you run this script when you don't mean to
+mv -v environment.yml "environment.yml.$(date +'%Y%m%dT%H%M%S')" #2>/dev/null
+IMAGE_NAME="doi-cookiecutter"
+docker build -t $IMAGE_NAME .
+docker run --rm -v `pwd`:/home/jovyan $IMAGE_NAME conda env export --no-builds --file environment.yml --name base #pip freeze > requirements.txt #
+# theoretically we are saving the env info so it can be recreated but it doesn't always work
